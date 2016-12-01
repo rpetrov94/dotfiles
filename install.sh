e5bbf30f3bfb3a10ccaf9ed49d3bf11892616172
Robert <rpetrov94@outlook.com>
2016-12-01 23:59:41 +0100

    updated install script again

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -Syu stow i3 i3status i3lock zsh vim xorg-xinit termite dunst compton
 
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+rm ~/.zshrc
 stow -d ~/.dotfiles zsh vim xinit dunst termite i3
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
